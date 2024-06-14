889ce5180d4fa401454082d5d3b5cec05c4591d5
Coral <coralovando1@upb.edu>
2024-06-14 10:01:57 -0400

    Descripción de los cambios realizados

--- main.py
+++ main.py
@@ -14,3 +14,44 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# import libraries
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# download nltk corpus (first time only)
+import nltk
+nltk.download('all')
+
+from huggingface_hub import hf_hub_download
+import pandas as pd
+dataset = pd.read_csv(
+    hf_hub_download(repo_id = 'jhan21/amazon-food-reviews-dataset', filename='Reviews.csv', repo_type="dataset")
+)
+print(dataset.head())
+
+# create preprocess_text function
+def preprocess_text(text):
+
+    # Tokenize the text
+    tokens = word_tokenize(text.lower())
+
+   # Remove stop words
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+   # Lemmatize the tokens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    # Join the tokens back into a string
+    processed_text = ' '.join(lemmatized_tokens)
+    return processed_text
+
+# apply the function df
+dataset['Text'] = dataset['Text'].apply(preprocess_text)
+
